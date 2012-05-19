6f85575778f18f9ce5b20963ce7900aea167b0b8
olibra <olibra@gmail.com>
2012-05-19 20:38:58 +0100

    ProjectPlan and Leveler updated

---
+++ TLogger/src/tLogger/Leveler.java
@@ -0,0 +1,14 @@
+package tLogger;
+
+public class Leveler{
+	
+	private String level;
+	
+	private void setLevel(int i){
+		switch (i){
+			case 0:	level = "WEVERE";
+			case 1:	level = "WARNING";
+			case 2: level = "ERROR";
+		}
+	}
+}
