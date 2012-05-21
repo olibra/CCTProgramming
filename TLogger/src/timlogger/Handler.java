9e367910f6c2d3c4b0e6ffb4e081ac8809f4beac
olibra <olibra@gmail.com>
2012-05-21 11:49:46 +0100

    tested write to file

--- TLogger/src/timlogger/Handler.java
+++ TLogger/src/timlogger/Handler.java
@@ -2,4 +2,29 @@ package timlogger;
 
 public class Handler {
 
+	private String where;
+	
+	private Handler(int i){
+		if (i == 0){
+			this.where = "Both";
+		}
+		if (i == 1){
+			this.where = "File";
+		}
+		if (i == 2){
+			this.where = "Console";
+		}
+	}
+	
+	private void write(int i){
+		if (i == 0){
+			this.where = "Both";
+		}
+		if (i == 1){
+			this.where = "File";
+		}
+		if (i == 2){
+			this.where = "Console";
+		}
+	}
 }
