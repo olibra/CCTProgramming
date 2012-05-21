ff142b54ed584d94ce1bd9f70f49169a87c8b310
olibra <olibra@gmail.com>
2012-05-21 15:48:00 +0100

    check class for get name with package and method

--- TLogger/Testing/BeepTesting.java
+++ TLogger/Testing/BeepTesting.java
@@ -1,32 +1,37 @@
+package timlogger;
+
+import java.util.Date;
 
 import java.awt.Toolkit;
 
 public class BeepTesting{
 
     public BeepTesting() throws InterruptedException{
-
+		
+    	whatIsMyName();
+		getPackageName();
  
 	        Toolkit.getDefaultToolkit().beep();
 	        
 	        Thread.sleep(2000);
 	        
 	        Toolkit.getDefaultToolkit().beep();
-	        Thread.sleep(500);
-	        
-	        Toolkit.getDefaultToolkit().beep();
-	        Thread.sleep(500);
-	        
-	        Toolkit.getDefaultToolkit().beep();
-	        Thread.sleep(500);
-	        
-	        Toolkit.getDefaultToolkit().beep();
-	        Thread.sleep(1000);
-	        
-	        Toolkit.getDefaultToolkit().beep();
     }
-
+    public void whatIsMyName() {
+        System.out.println(
+ 	           Thread.currentThread().getStackTrace()[1].getMethodName()); // output : doit
+ 	     }
+	public void getPackageName(){
+		     Package pack = (new Date()).getClass().getPackage();
+		     String packageName = pack.getName();
+		     System.out.println("Package Name = " + packageName);
+	     }
 
     public static void main(String[] args) throws InterruptedException{
-    	new BeepTesting();
+
+		BeepTesting o = new BeepTesting();
+		String packageName = o.getClass().getPackage().getName();
+		System.out.println("Package Name = " + packageName);
+		
     }
 }
